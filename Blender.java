42c668199202e70211ed29ad8c141cf5e4bd4030
juanfriorozco8 <oro24647@uvg.edu.gt>
2025-01-22 12:11:40 -0600

    metodo constructor, getters & setters

--- Blender.java
+++ Blender.java
@@ -1,3 +1,28 @@
 public class Blender {
-    
+   private int speedLevel;
+   private int capacity;
+
+
+    public Blender(int speedLevel, int capacity) {
+        this.speedLevel = speedLevel;
+        this.capacity = capacity;
+    }
+
+
+    public int getSpeedLevel() {
+        return this.speedLevel;
+    }
+
+    public void setSpeedLevel(int speedLevel) {
+        this.speedLevel = speedLevel;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
 }
