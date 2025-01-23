8030d4d5abdf14c48f5113f1db743c2f551dd4b8
marianacastanedar <167562546+marianacastanedar@users.noreply.github.com>
2025-01-22 21:51:27 -0600

    Finalizar el programa, ya funciona todo el Main

--- Main.java
+++ Main.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Blender licuadora1 = new Blender(0, 0);
+        Blender licuadora1 = new Blender(6, 0);
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("1. Prende la licuadora\r\n" + //
                         "2. Llena la licuadora con lo que se desea licuar\r\n" + //
                         "3. Incrementa velocidad\r\n" + //
-                        "4. Consulta en que velocidad esta la licuadora\r\n" + //
-                        "5. Consulta si la licuadora esta llena.\r\n" + //
-                        "6. Vaciar la licuadora\r\n" + //
-                        "7. Salir \r\n" + //
-                        "8. menos velocidad"
+                        "4. Disminuye velocidad \r\n" + //
+                        "5. Consulta en que velocidad esta la licuadora\r\n" + //
+                        "6. Consulta si la licuadora esta llena.\r\n" + //
+                        "7. Vaciar la licuadora\r\n" + //
+                        "8. Salir \r\n" 
                         );
         
         Boolean seguir = true;
@@ -33,29 +33,36 @@ public class Main {
                 licuadora1.SpeedUp();
             }
             if (opcion == 4) {
-                licuadora1.GetSpeed();
+                licuadora1.SpeedDown();
             }
             if (opcion == 5) {
+                if (licuadora1.GetSpeed() == 0) {
+                    System.out.println("La licuadora está apagada :)");
+                } else if(licuadora1.GetSpeed() == licuadora1.getSpeedMax()){
+                    System.out.println("La licuadora está en la Máxima Velocidad!, que es: " + licuadora1.GetSpeed());
+                }else {
+                    System.out.println("La licuadora está en la velocidad: " + licuadora1.GetSpeed());
+                }
+            }
+            if (opcion == 6) {
                 if (licuadora1.IsFull() == true) {
                     System.out.println("La licuadora está llena");
                 } else {
                     System.out.println("La licuadora no está llena");
                 }
             }
-            if (opcion == 6) {
+            if (opcion == 7) {
                 licuadora1.Empty();
                 System.out.println("La licuadora no está llena");
             }
-            if (opcion == 7) {
+            if (opcion == 8) {
                 System.out.println("Saliendo... Gracias por usar el programa");
                 seguir = false;
             }
-            if (opcion > 7 ||  opcion < 1) {
+            if (opcion > 8 ||  opcion < 1) {
                 System.out.println("Ingrese una opción válida");
             }
-            if (opcion == 8) {
-                licuadora1.SpeedDown();
-            }
+            
         }
         
     }
