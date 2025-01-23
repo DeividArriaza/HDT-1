cde91e6319a67c6f9d46ede3ee163e49b79b6056
marianacastanedar <167562546+marianacastanedar@users.noreply.github.com>
2025-01-22 18:17:09 -0600

    resolucion de errores en clase Blender y Main

--- Blender.java
+++ Blender.java
@@ -4,11 +4,7 @@ public class Blender implements IBlender {
 
    private int speedMax;
    private int capacity;
-<<<<<<< HEAD
    private int SpeedLevel;
-=======
-   
->>>>>>> 2d638db5296a73460bf1e9c713775b9534a5bd2a
 
 
     public Blender(int speedMax, int capacity) {
@@ -34,19 +30,8 @@ public class Blender implements IBlender {
         this.capacity = capacity;
     }
 
-<<<<<<< HEAD
     public void setSpeedLevel(int speedLevel) {
         SpeedLevel = speedLevel;
-=======
-    Blender blender = new Blender(0, 50);
-
-    @Override
-    public void SpeedUp(){
-        if (speedLevel < 10){
-        speedLevel = blender.getSpeedLevel() + 1;
-        System.out.println("Nivel");
-        }
->>>>>>> 2d638db5296a73460bf1e9c713775b9534a5bd2a
     }
 
 
